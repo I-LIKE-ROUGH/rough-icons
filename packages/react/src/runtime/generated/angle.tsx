c30dbede8089d2880d67d1522358e67e51e55520
syi0808 <66503450+syi0808@users.noreply.github.com>
2026-08-10 19:21:22 +0000

    chore: sync Lucide icons

---
+++ packages/react/src/runtime/generated/angle.tsx
@@ -0,0 +1,3 @@
+import { AngleSource as source } from '@rough-lucide/icons/source/angle';
+import { createRuntimeIcon } from '../../create-runtime-icon.js';
+export const Angle = createRuntimeIcon(source);
---
+++ packages/react/src/static/generated/mail-badge.tsx
@@ -0,0 +1,3 @@
+import { MailBadge as data } from '@rough-lucide/icons/icons/mail-badge';
+import { createStaticIcon } from '../../create-static-icon.js';
+export const MailBadge = createStaticIcon(data);
